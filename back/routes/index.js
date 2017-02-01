21ecda3626734c41727ca26082890638ffb29da5
bigkangtheory <lisalkang@gmail.com>
2017-02-01 12:18:59 -0500

    edit routes and start seed test files

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 
-//present URL is /api
-router.use('/sample', require('./sample-router'))
-
-//lisa's itinerary example (add yours below)
 router.use('/itinerary', require('./itinerary-router'));
 
+
+module.exports = router;
