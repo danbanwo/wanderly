5d81aa0a02dff07e6cdd72dd906a25beabdaf75d
bigkangtheory <lisalkang@gmail.com>
2017-02-01 11:30:06 -0500

    add eslint ignore

--- back/routes/sample-router.js
+++ back/routes/sample-router.js
@@ -2,7 +2,6 @@ const sampleRouter = require('express').Router();
 //require in the model and the associated models
 const model = require('../models').Model(); //whatever you defined your model as in the model.js
 
-
 //create the functions for the API calls
 const blah = (req, res) => {
 	model.findAll()
