efacfbdf54445bab2b01ba4a837542068fe2365e
Tionna Wilson-Pierce <tionna.wilsonpierce@gmail.com>
2017-02-01 11:49:50 -0500

    add seed folder

--- back/models/profile-model.js
+++ back/models/profile-model.js
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Profile.hasMany(models.Adventure)
+        // Profile.hasMany(models.Adventure)
       }
     }
   });
---
+++ back/seed/profile-seed.js
@@ -0,0 +1,45 @@
+[{
+  "first_name":"Emily",
+  "last_name":"Anderson",
+  "gender":"Female",
+  "age":18,
+  "photo":"http://dummyimage.com/232x120.png/dddddd/000000",
+  "country_origin":"China",
+  "phrase":"Intuitive non-volatile function"
+},
+{
+  "first_name":"Ruby",
+  "last_name":"Mason",
+  "gender":"Female",
+  "age":71,
+  "photo":"http://dummyimage.com/184x159.png/cc0000/ffffff",
+  "country_origin":"Indonesia",
+  "phrase":"Focused actuating secured line"
+},
+{
+  "first_name":"Pamela",
+  "last_name":"Campbell",
+  "gender":"Female",
+  "age":81,
+  "photo":"http://dummyimage.com/176x230.jpg/cc0000/ffffff",
+  "country_origin":"Moldova",
+  "phrase":"Quality-focused bottom-line support"
+},
+{
+  "first_name":"Judith",
+  "last_name":"Woods",
+  "gender":"Female",
+  "age":96,
+  "photo":"http://dummyimage.com/183x110.png/ff4444/ffffff",
+  "country_origin":"Indonesia",
+  "phrase":"Diverse analyzing matrices"
+},
+{
+  "first_name":"Kevin",
+  "last_name":"Day",
+  "gender":"Male",
+  "age":71,
+  "photo":"http://dummyimage.com/232x145.bmp/cc0000/ffffff",
+  "country_origin":"Indonesia",
+  "phrase":"Ameliorated grid-enabled installation"
+}]
