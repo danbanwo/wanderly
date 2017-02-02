0d133ce6123863bc282d2c5e9f5a3962f0934cda
Daniel <danbanwo@gmail.com>
2017-02-02 11:49:25 -0500

    add map feature using react-google-maps

--- back/routes/sample-router.js
+++ back/routes/sample-router.js
@@ -1,6 +1,6 @@
 const sampleRouter = require('express').Router();
 //require in the model and the associated models
-const model = require('../models').Model(); //whatever you defined your model as in the model.js
+const model = require('../models').Model; //whatever you defined your model as in the model.js
 
 
 //create the functions for the API calls
