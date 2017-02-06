02d0b244b2fd8c150c86facf735e2fe4d7d19192
Daniel <danbanwo@gmail.com>
2017-02-06 16:11:23 -0500

    create profile component, add map and itinerary box to profile component

--- front/app/entry.jsx
+++ front/app/entry.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import {browserHistory, IndexRoute, Router, Route} from 'react-router';
-// import Map from './map/map'
-import MarkerClustererExample from './map/sample-map'
 
 
 //import components
-import Map from './map/map';
-import ItineraryBox from './itinerary/itinerary-container';
+// import Map from './map/map';
+import Profile from './profile/profile';
 import SignUp from './signup/signup.jsx';
 
 
@@ -20,9 +18,7 @@ let App = React.createClass({
     return(
       <div>
         <h1>Welcome to Wanderly!</h1>
-        <div style={{width: 500, height:300, background: 'red'}}>
-          <MarkerClustererExample center={location}/>
-        </div>
+        {this.props.children}
       </div>
     )
   }
@@ -32,7 +28,7 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route path='/' component={App} />
     <Route path='/signup' component={SignUp}/>
-    <Route path='/itinerarytest' component={ItineraryBox} />
+    <Route path='/home' component={Profile} />
   </Router>,
   document.getElementById('root')
 )
