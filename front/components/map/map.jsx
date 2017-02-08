2fc5c1f14d118df8b8e786e6a323203d79173221
Daniel <danbanwo@gmail.com>
2017-02-08 15:47:00 -0500

    add lat and lng to Destination model. Create association hasMany with WanderSpot model

--- front/components/map/map.jsx
+++ front/components/map/map.jsx
@@ -1,60 +1,57 @@
-import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
-import fetch from "isomorphic-fetch";
-
-import { test } from '../../actions/map-actions'
-
+import React, { Component, } from 'react';
+import { withGoogleMap, GoogleMap, Marker, } from 'react-google-maps';
+import fetch from 'isomorphic-fetch';
 
 // import MarkerClusterer from "../../../lib/addons/MarkerClusterer";
 
 const WanderGoogleMap = withGoogleMap(props => (
   <GoogleMap
     defaultZoom={3}
-    defaultCenter={{ lat: 25.0391667, lng: 121.525 }}
+    defaultCenter={{ lat: 25.0391667, lng: 121.525, }}
   >
     {/* <MarkerClusterer
       averageCenter
       enableRetinaIcons
       gridSize={60}
     > */}
-      {props.markers.map((marker, i) => {
-        if(i < 10 ) {
-          return <Marker
-            position={{ lat: marker.latitude, lng: marker.longitude }}
-            key={marker.photo_id}
-          />
-        }
-      })}
+    {props.markers.map((marker, i) => {
+      if (i < 10) {
+        return (<Marker
+          position={{ lat: marker.latitude, lng: marker.longitude, }}
+          key={marker.photo_id}
+        />);
+      }
+    })}
     {/* </MarkerClusterer> */}
   </GoogleMap>
 ));
 
 export default class WanderMap extends Component {
-constructor(props){
-  super(props)
-  this.state = {
-    markers: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [],
+    };
   }
-}
 
   componentDidMount() {
-    fetch(`https://gist.githubusercontent.com/farrrr/dfda7dd7fccfec5474d3/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json`)
+    fetch('https://gist.githubusercontent.com/farrrr/dfda7dd7fccfec5474d3/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json')
       .then(res => res.json())
-      .then(data => {
-        this.setState({ markers: data.photos });
+      .then((data) => {
+        this.setState({ markers: data.photos, });
       });
     // this.props.markers()
   }
 
   render() {
-    console.log("PROPS========>", this.props)
+    console.log('PROPS========>', this.props);
     return (
       <WanderGoogleMap
         containerElement={
-          <div style={{ height: `100%` }} />
+          <div style={{ height: '100%', }} />
         }
         mapElement={
-          <div style={{ height: `100%` }} />
+          <div style={{ height: '100%', }} />
         }
         markers={this.state.markers}
       />
