2a8a267f8208f676c2dc7f79dba4b193ff5573d6
bigkangtheory <lisalkang@gmail.com>
2017-02-10 17:29:56 -0500

    seeder

--- back/seed/wanderspot-seed.js
+++ back/seed/wanderspot-seed.js
@@ -1,5 +1,6 @@
-var Wanderspot = require('../models').Wanderspot;
+'use strict'
 
+var Wanderspot = require('../models').Wanderspot;
 
 let wanderspotArr = [
   {
@@ -31,7 +32,6 @@ let wanderspotArr = [
     total: 12,
     category: 'play',
     DestinationId: 1
-
   },
   {
     pictures: 'https://s-media-cache-ak0.pinimg.com/564x/2a/c2/e8/2ac2e890e8e610782a83dde48b0114fd.jpg',
@@ -92,6 +92,216 @@ let wanderspotArr = [
     total: 1000,
     category: 'sleep',
     DestinationId: 2
+  },
+  {
+    pictures: 'https://s-media-cache-ak0.pinimg.com/originals/b3/aa/4e/b3aa4e422bf95afb8335b5edffd0f6f7.jpg',
+    description: "Seriously the best gelato ice cream ever!",
+    spot: 'Gelateria La Carraia',
+    lng: 11.246673,
+    lat: 43.769643,
+    total: 4,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://cleocollective.com/wp-content/uploads/2015/03/Gelateria-dei-Neri-Best-Gelato-in-Florence-Tuscany-1024x768.jpg',
+    description: "Okay florence has the best gelato in Italy for sure. Soooo much ice cream so cheap!",
+    spot: 'Gelateria de Neri',
+    lng: 11.259098,
+    lat: 43.767764,
+    total: 4,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://cdn1.discovertuscany.com/img/florence/museums/botticelli-venus-uffizi.jpg?w=1140&q=60&auto=enhance',
+    description: "I just saw classical paintings IRL. WOW.",
+    spot: 'Uffizi Gallery',
+    lng: 11.255311,
+    lat: 43.767786,
+    total: 30,
+    category: 'play',
+    DestinationId: 2
+  },
+  {
+    pictures: 'https://wheninflorenceblog.files.wordpress.com/2013/07/all-antico-vinaio.jpg',
+    description: "HUGE paninis for only 10euros. One is definitely enough for 2 people.",
+    spot: 'SandwiChic',
+    lng: 11.256195,
+    lat: 43.777164,
+    total: 10,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'https://a0.muscache.com/im/pictures/33679999/2bee78f0_original.jpg?aki_policy=xx_large',
+    description: "Stayed at this beautiful Airbnb that was close to everything",
+    spot: 'Florence',
+    lng: 11.253165,
+    lat: 43.768009,
+    total: 120,
+    category: 'sleep',
+    DestinationId: 2
+  },
+  {
+    pictures: 'https://a0.muscache.com/im/pictures/14936106/ab62bd73_original.jpg?aki_policy=xx_large',
+    description: "My Airbnb",
+    spot: 'Venice',
+    lng: 12.328463,
+    lat: 45.444958,
+    total: 73,
+    category: 'sleep',
+    DestinationId: 2
+  },
+  {
+    pictures: 'https://a0.muscache.com/im/pictures/14936106/ab62bd73_original.jpg?aki_policy=xx_large',
+    description: "Gondola ride! A half hour ride was 80 euros.",
+    spot: 'Grand Canal',
+    lng: 12.328463,
+    lat: 45.444958,
+    total: 80,
+    category: 'play',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://www.vancouversun.com/cms/binary/8865305.jpg',
+    description: "Risotto noodles with squid ink... very interesting.",
+    spot: 'Risotto al nero di seppia',
+    lng: 12.322569,
+    lat: 45.433340,
+    total: 80,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://slice.seriouseats.com/images/2013/01/20121002-napoli-pizza-tour-35.jpg',
+    description: "I looove pizza",
+    spot: 'Pizzeria Di Matteo',
+    lng: 14.257968,
+    lat: 40.851251,
+    total: 6,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://slice.seriouseats.com/images/2013/01/20121002-napoli-pizza-tour-12.jpg',
+    description: "Can you believe pizza is their normal daily meal?",
+    spot: 'Starita',
+    lng: 14.246381,
+    lat: 40.855923,
+    total: 6,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://slice.seriouseats.com/images/2013/01/20121002-napoli-pizza-tour-24.jpg',
+    description: "Julia Roberts was here!",
+    spot: 'Da Michele',
+    lng: 14.263303,
+    lat: 40.849753,
+    total: 5.50,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://slice.seriouseats.com/images/2013/01/20121002-napoli-pizza-tour-32.jpg',
+    description: "Pizza pizza pizza! I got a fresh mozz topping one.",
+    spot: 'Pizzeria Presidente',
+    lng: 14.259192,
+    lat: 40.851642,
+    total: 8,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://slice.seriouseats.com/images/2013/01/20121002-napoli-pizza-tour-04.jpg',
+    description: "This is supposedly the first pizzeria in the world.",
+    spot: 'ANTICA PIZZERIA PORT ALBA',
+    lng: 14.251753,
+    lat: 40.849372,
+    total: 5,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://slice.seriouseats.com/images/2013/01/20121002-napoli-pizza-tour-17.jpg',
+    description: "So much action with all the toppins and workers.",
+    spot: 'PIZZERIA TRIANON DA CIRO',
+    lng: 14.263687,
+    lat: 40.850057,
+    total: 6,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+    pictures: 'http://slice.seriouseats.com/images/2013/01/20121002-napoli-pizza-tour-56.jpg',
+    description: "I looove pizza",
+    spot: 'EUROPEO DI MATTOZZI',
+    lng: 14.255806,
+    lat: 40.843349,
+    total: 5,
+    category: 'eat',
+    DestinationId: 2
+  },
+  {
+   pictures: 'http://www.emmajanenation.com/wp-content/uploads/2013/10/De-Kas-Amsterdam_0018.jpg',
+   description: "They grow their ingredients in-house. This restaurant is a green house!",
+   spot: 'De Kas',
+   lng: 4.930286,
+   lat: 52.352200,
+   total: 50,
+   category: 'eat',
+   DestinationId: 3
+  },
+  {
+   pictures: 'http://albertcuyp-markt.amsterdam/wp-content/uploads/2015/12/original-stroopwafels-header-2.jpg',
+   description: "Have you tried a stroopwafel after visiting a coffeeshop? It's a must",
+   spot: 'Original Stroopwafels',
+   lng: 4.895345,
+   lat: 52.355957,
+   total: 2,
+   category: 'eat',
+   DestinationId: 3
+  },
+  {
+   pictures: 'http://www.facetourist.com/uploads/2/5/6/1/25616865/wallen-amsterdam-red-light-district-tour-trip-vacation-travel-traveling_orig.jpg',
+   description: "Very Moulin Rougey...",
+   spot: 'De Wallen',
+   lng: 4.895861,
+   lat: 52.371860,
+   total: 0,
+   category: 'play',
+   DestinationId: 3
+  },
+  {
+   pictures: 'https://cdn4.vtourist.com/4/7639989-Anne_Frank_House_Thought_Provoking_Museum.jpg?version=2',
+   description: "No student discounts but it was worth the full price. Tragic story and history is felt.",
+   spot: 'Anne Frank House',
+   lng: 4.883977,
+   lat: 52.375218,
+   total: 20,
+   category: 'play',
+   DestinationId: 3
+  },
+  {
+   pictures: 'http://trojantravel.usc.edu/wp-content/uploads/2015/07/Peru_Machu-Picchu-Llamas.jpg',
+   description: "HIKED THIS BAD BOY!",
+   spot: 'Machu Picchu',
+   lng: -72.544963,
+   lat: -13.163141,
+   total: 55,
+   category: 'play',
+   DestinationId: 3
+  },
+  {
+   pictures: 'http://pix.avaxnews.com/avaxnews/c4/bb/0001bbc4_medium.jpeg',
+   description: "Almost at the boarder of chile!",
+   spot: 'The Andes',
+   lng: -66.775248,
+   lat: -21.160767,
+   total: 20,
+   category: 'play',
+   DestinationId: 3
   },
 ];
 
