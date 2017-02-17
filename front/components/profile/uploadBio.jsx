5b8f185192df337f2309349f9fa8e831e2596cfc
Tionna Wilson-Pierce <tionna.wilsonpierce@gmail.com>
2017-02-17 13:49:13 -0500

    updated css

--- front/components/profile/uploadBio.jsx
+++ front/components/profile/uploadBio.jsx
@@ -7,26 +7,34 @@ const UploadBio = React.createClass({
 	render(){
 		return(
 		<div>
+
+		<div className="bioContainer">
 		<center>
-		<h1> Welcome, traveler!</h1>
+		<h1 className="bioTitle"> Welcome, traveler!</h1>
 		<h3> Tell us a little more about yourself. </h3>
 
-		<p> What types of travel do you like? </p>
-			<button type="button">Category</button>
-			<button type="button">Category</button>
-			<button type="button">Category</button>
-			<button type="button">Category</button>
-			<button type="button">Category</button>
-			<button type="button">Category</button>
 
-		<p> Introduce yourself </p>
+		<div className="bioQuestions">
+		<p id="questions"> What types of travel do you like? </p>
+			<button type="button" className="btn btn-default one">Food</button>
+			<button type="button" className="btn btn-default two">Culture & Art</button>
+			<button type="button" className="btn btn-default three">Relaxation</button>
+			<button type="button" className="btn btn-default four">Backpacking</button>
+			<button type="button" className="btn btn-default five">Activities</button>
+			<button type="button" className="btn btn-default six">Independence</button>
+		</div>
 
-		<input type="text" placeholder="Write a brief intro..." className="bio"></input>
 
-		<button type="button">All done!</button>
+		<div className="bioIntro">
+		<p className="questions"> Introduce yourself </p>
+		<input type="text" placeholder="Write a brief intro..." className="intro"></input>
+		</div>
 
+
+		<button className="btn btn-default done" type="button">All done!</button>
 		<a href="http://localhost:3000/home" id="skip">Skip this step</a>
 		</center>
+		</div>
 
 		</div>
 		)
