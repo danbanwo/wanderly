c6cd25bab85546201ce9c85af98d1427a600f447
Daniel <danbanwo@gmail.com>
2017-02-25 16:43:49 -0500

    upload feature working

--- front/components/profile/profile.jsx
+++ front/components/profile/profile.jsx
@@ -28,6 +28,7 @@ class Profile extends Component {
   }
 
   render() {
+    console.log(this.props.profile.image)
     return (
 
       <div id='master-container'>
--- front/components/profile/uploadComponent.js
+++ front/components/profile/uploadComponent.js
@@ -4,12 +4,27 @@ import axios from 'axios';
 
 
 class UploadComponent extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	handleSubmit= (e) => {
+		e.preventDefault()
+		let form = document.getElementById('form')
+		var fileInput = document.getElementById('file');
+		var file = fileInput.files[0];
+		var formData = new FormData(form);
+		formData.append('file', file);
+		console.log(formData)
+		this.props.uploadImage(formData)
+	}
+
 	render() {
     const { pathname } = this.props.pathname
     if(pathname === '/uploadpic') {
   		return (
   			<div className="uploadContainer">
-  				<form action="/api/profile/upload" encType="multipart/form-data" method="post">
+  				<form onSubmit={this.handleSubmit} id='form'>
   					<input id="file" type="file" name="upload" multiple/>
             <input className="btn btn-default" type="submit" value="Upload photo"/>
   				</form>
